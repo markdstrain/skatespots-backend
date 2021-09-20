ae87001fcab0c4fef7acac1211c99a296a654b72
Mark Strain <markdstrain@gmail.com>
2021-09-19 19:58:36 -0500

    removing cached git info

--- skatespots.sql
+++
@@ -1,9 +0,0 @@
-\echo 'Delete and recreate skate-spot db?'
-\prompt 'Return for yes or control-C to cancel >' foo
-
-DROP DATABASE skatespots;
-CREATE DATABASE skatespots;
-\connect skatespots
-
-\i skatespots-schema.sql
-\i skatespots-seed.sql
